264a1fd5abbf796e252b862a4b6551d21fb1c093
patrickppg <148911314+patrickppg@users.noreply.github.com>
2024-02-19 10:50:49 -0300

    helper functions and unit tests - step 3

--- part-4/helper_functions_and_unit_test/utils/list_helper.js
+++ part-4/helper_functions_and_unit_test/utils/list_helper.js
@@ -6,7 +6,13 @@ function totalLikes(blogs) {
   return blogs.reduce((likes, blog) => likes + blog.likes, 0)
 }
 
+function favoriteBlog(blogs) {
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  return blogs.find(el => el.likes === maxLikes)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
