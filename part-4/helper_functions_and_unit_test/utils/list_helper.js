936cb7467e74ae2d4610bf322deabb25eaf3dd69
patrickppg <148911314+patrickppg@users.noreply.github.com>
2024-02-19 10:22:36 -0300

    helper functions and unit tests - step 2

--- part-4/helper_functions_and_unit_test/utils/list_helper.js
+++ part-4/helper_functions_and_unit_test/utils/list_helper.js
@@ -2,6 +2,11 @@ function dummy(blogs) {
   return 1
 }
 
+function totalLikes(blogs) {
+  return blogs.reduce((likes, blog) => likes + blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
