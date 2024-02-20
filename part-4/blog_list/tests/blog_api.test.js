53fcda00bae193eb30c9c582a649eaaae2fbccec
patrickppg <148911314+patrickppg@users.noreply.github.com>
2024-02-20 18:48:55 -0300

    bloglist tests - step 2

--- part-4/blog_list/tests/blog_api.test.js
+++ part-4/blog_list/tests/blog_api.test.js
@@ -8,20 +8,16 @@ const mongoose = require('mongoose')
 
 const initialBlogs = [
   {
-    _id: "5a422a851b54a676234d17f7",
     title: "React patterns",
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
-    __v: 0
   },
   {
-    _id: "5a422aa71b54a676234d17f8",
     title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
-    __v: 0
   },
 ]
 
@@ -43,6 +39,23 @@ test('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
   assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('returned saved blog has an id property', async () => {
+  const newBlog = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  }
+
+  const res = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  
+  assert(res.body.id)
 })
 
 after(async () => {
