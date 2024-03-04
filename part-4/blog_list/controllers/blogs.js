4bae944c64101635c715d9878b90049f337334ff
patrickppg <148911314+patrickppg@users.noreply.github.com>
2024-03-04 14:54:10 -0300

    bloglist expansions - step 5

--- part-4/blog_list/controllers/blogs.js
+++ part-4/blog_list/controllers/blogs.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const router = express.Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 router.use(express.json())
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { name: 1, username: 1 })
   res.json(blogs)
 })
 
 router.post('/', async (req, res, next) => {
   const body = req.body
+  const user = await User.findOne({})
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes ?? 0
+    likes: body.likes ?? 0,
+    user: user.id
   })
 
   try {
     const createdBlog = await blog.save()
+    user.blogs.push(createdBlog._id)
+    await user.save()
     res.status(201).json(createdBlog)
   } catch (err) {
     next(err)
--- part-4/blog_list/models/user.js
+++ part-4/blog_list/models/user.js
@@ -8,6 +8,12 @@ const userSchema = new mongoose.Schema({
     unique: true
   },
   passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ]
 })
 
 userSchema.set('toJSON', {
